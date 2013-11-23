daca042ae2f0a7d2cd65861c24e4c49752878333
Erkan Gürel <erkangurel@gmail.com>
2013-11-23 15:14:52 +0200

    donation1

--- dotatr/donation/urls.py
+++ dotatr/donation/urls.py
@@ -4,4 +4,5 @@ from donation import views
 
 urlpatterns = patterns('',
     url(r'^$', views.donate_page, name='donate_page')
+    url(r'^success/', views.donate_page_success, name='donate_page_success')
 )
--- dotatr/donation/views.py
+++ dotatr/donation/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from pprint import pprint
 
 def donate_page(request):
 	return render(request, 'donation/donation_page.html', {})
+
+def donate_page_success(request):
+	#burda bilgiler gelicek
+	
+	pprint (vars(request))
+	return render(request, 'donation/donate_page_success.html', {})
