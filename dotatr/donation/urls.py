e0975f9f764aad855f15a75ab3bc98b204474869
Erkan Gürel <erkangurel@gmail.com>
2013-11-23 15:36:28 +0200

    donation1

--- dotatr/donation/urls.py
+++ dotatr/donation/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from donation import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.donate_page, name='donate_page')
-    url(r'^success/$', views.donate_page_success, name='donate_page_success')
+    url(r'^$', views.donate_page, name='donate_page'),
+    url(r'^success/$', views.donate_page_success, name='donate_page_success'),
 )
