c2e83f80b173f429f54040a4fc9f4b559c4eec3d
Erkan Gürel <erkangurel@gmail.com>
2013-11-24 20:43:39 +0200

    donation

--- dotatr/donation/views.py
+++ dotatr/donation/views.py
@@ -4,21 +4,20 @@ import json, base64, requests, urlparse
 pdt_hash = "9Le9iJBWqevVdfaOUAx3XT3kDHrGXUnNmzcABlkJKNqIxHKCt_xTj9uho7G"
 
 def donate_page(request):
-	custom_value = {'user':"", 'game':"dota2"} 
+	custom_value = {'user':"erkan", 'game':"dota2"} 
 	custom_json = json.dumps(custom_value);
 	custom_obfuscated = base64.standard_b64encode(custom_json)
 
 	return render(request, 'donation/donation_page.html', {'custom': custom_obfuscated, 'o':"sometext"})
 
-def donate_page_success(req):
-	getObj = dict(req.GET.iterlists())
+def donate_page_success(request):
+	pdt = getPDT(request)
 
-	payload = {'cmd':"_notify-synch", 'tx':getObj[u'tx'], 'at': pdt_hash}
-	response = requests.get('https://www.sandbox.paypal.com/cgi-bin/webscr', params=payload)
+	custom = json.loads(base64.standard_b64decode(pdt.custom))
 
-	pdt = parsePDT(response.content)
+	print custom['user']
 
-	return render(req, 'donation/donation_page_success.html', {'response': pdt})
+	return render(request, 'donation/donation_page_success.html', {'response': pdt})
 
 
 def parsePDT(pdt):
@@ -31,3 +30,10 @@ def parsePDT(pdt):
 
 	return resultPDT
 
+def getPDT(req):
+	getObj = dict(req.GET.iterlists())
+	payload = {'cmd':"_notify-synch", 'tx':getObj[u'tx'], 'at': pdt_hash}
+	response = requests.get('https://www.sandbox.paypal.com/cgi-bin/webscr', params=payload)
+	pdt = parsePDT(response.content)
+
+	return pdt	
