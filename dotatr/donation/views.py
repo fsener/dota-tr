36bf209b8f8c247350e51c46920313f32074229c
Erkan Gürel <erkangurel@gmail.com>
2013-11-24 19:14:08 +0200

    donation

--- dotatr/donation/views.py
+++ dotatr/donation/views.py
@@ -3,6 +3,8 @@ import json
 import base64
 import requests
 
+pdt_hash = "9Le9iJBWqevVdfaOUAx3XT3kDHrGXUnNmzcABlkJKNqIxHKCt_xTj9uho7G"
+
 def donate_page(request):
 	custom_value = {'user':"", 'game':"dota2"} 
 	custom_json = json.dumps(custom_value);
@@ -10,5 +12,8 @@ def donate_page(request):
 
 	return render(request, 'donation/donation_page.html', {'custom': custom_obfuscated, 'o':"sometext"})
 
-def donate_page_success(request):
-	return render(request, 'donation/donation_page_success.html', {'tx': request.GET, 'o':'o'})
+def donate_page_success(req):
+	payload = {'cmd':"_notify-synch", 'tx':req.GET.tx, 'at': pdt_hash}
+	response = requests.get('https://www.sandbox.paypal.com/cgi-bin/webscr')
+
+	return render(request, 'donation/donation_page_success.html', {'response': response})
