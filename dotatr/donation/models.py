05d52b8860018fca0bdb35b42fe7ed3ad48a77c5
Erkan Gürel <erkangurel@gmail.com>
2013-11-25 15:02:06 +0200

    donation

--- dotatr/donation/models.py
+++ dotatr/donation/models.py
@@ -6,7 +6,7 @@ class DonationTarget(models.Model):
 	current = models.IntegerField()
 	end_date = models.DateTimeField("End Date", auto_now_add=True)
 	start_date = models.DateTimeField("Start Date")
-	active = models.BooleanField(initial=False)
+	active = models.BooleanField(default=False)
 
 	def __unicode__(self):
 		return self.month
